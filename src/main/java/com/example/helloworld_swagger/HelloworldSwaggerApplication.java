cee313bf80092859c3d4f64adbf4788955a49bb4
Erik Hernandez <erikestuardo_161@hotmail.com>
2024-09-26 20:14:14 -0600

    Add All Files Swager

---
+++ src/main/java/com/example/helloworld_swagger/HelloworldSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworld_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldSwaggerApplication.class, args);
+	}
+
+}
